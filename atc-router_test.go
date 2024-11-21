3c1241e5fc3ba697106400bb701215a61301fcce
Javier <javier.guerra@konghq.com>
2024-11-21 09:21:07 -0500

    feat: expression_validate function (#6)
    * feat: expression_validate function
    
    New `expression_validate` function in atc-router library.
    Can validate an expression without defining a `Router` object.
    If valid, returns the set of fields and operators used by the
    expression.
    
    * add test to low level util function
    
    * updated to latest atc-router/KAG-5013
    
    ---------
    
    Signed-off-by: Javier Guerra <javier.guerra@konghq.com>

--- atc-router_test.go
+++ atc-router_test.go
@@ -1,18 +1,29 @@
 package goatcrouter
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 )
 
-func verify(atc string) error {
-	schema := NewSchema()
-	defer schema.Free()
+type field struct {
+	f string
+	t FieldType
+}
 
-	schema.AddField("http.path", String)
-	schema.AddField("tcp.port", Int)
+func newSchema(fields ...field) *Schema {
+	schema := NewSchema()
+	for _, fld := range fields {
+		schema.AddField(fld.f, fld.t)
+	}
+	return schema
+}
+
+func verify(atc string) error {
+	schema := newSchema(field{"http.path", String}, field{"tcp.port", Int})
+	defer schema.Free()
 
 	router := NewRouter(schema)
 	defer router.Free()
@@ -21,11 +32,8 @@ func verify(atc string) error {
 }
 
 func get_fields(atc string) ([]string, error) {
-	schema := NewSchema()
+	schema := newSchema(field{"http.path", String}, field{"tcp.port", Int})
 	defer schema.Free()
-
-	schema.AddField("http.path", String)
-	schema.AddField("tcp.port", Int)
 
 	router := NewRouter(schema)
 	defer router.Free()
@@ -55,4 +63,45 @@ func Test_GetFields(t *testing.T) {
 	fields, err = get_fields(`tcp.port == 1 && http.path==""`)
 	require.NoError(t, err)
 	require.ElementsMatch(t, []any{"http.path", "tcp.port"}, fields)
+}
+
+func Test_splitByNulls(t *testing.T) {
+	for _, test := range []struct {
+		name string
+		buf  []byte
+		n    int
+		out  []string
+	}{
+		{name: "empty input", buf: nil, out: []string{}},
+		{name: "empty input, n:1", buf: nil, n: 1, out: []string{""}},
+		{name: "single string", buf: []byte("one\x00"), n: 1, out: []string{"one"}},
+		{name: "single string, n:0", buf: []byte("one\x00"), n: 0, out: []string{}},
+		{name: "single string, n:2", buf: []byte("one\x00"), n: 2, out: []string{"", "one"}},
+		{name: "unterminated", buf: []byte("one"), n: 1, out: []string{""}},
+		{name: "two, unterminated", buf: []byte("one\x00two"), n: 2, out: []string{"", "one"}},
+		{name: "consecutive separator 3", buf: []byte("one\x00\x00two\x00"), n: 3, out: []string{"", "one", "two"}},
+		{name: "consecutive separator 2", buf: []byte("one\x00\x00two\x00"), n: 2, out: []string{"", "one"}},
+	} {
+		t.Run(fmt.Sprintf("splitByNulls %q:", test.name), func(t *testing.T) {
+			out := splitByNulls(test.buf, test.n)
+			require.Equal(t, test.n, len(out))
+			require.EqualValues(t, test.out, out)
+		})
+	}
+}
+
+func Test_ValidateExpression(t *testing.T) {
+	schema := newSchema(field{"http.path", String}, field{"tcp.port", Int})
+	defer schema.Free()
+
+	res := ValidateExpression(*schema, "tcp.port == 1")
+	require.Equal(t, &ValidationResult{
+		fields:    []string{"tcp.port"},
+		operators: BinaryOperatorFlags_EQUALS,
+	}, res)
+
+	require.Equal(t, &ValidationResult{
+		fields:    []string{"http.path", "tcp.port"},
+		operators: BinaryOperatorFlags_EQUALS + BinaryOperatorFlags_NOT_EQUALS,
+	}, ValidateExpression(*schema, `tcp.port == 1 && http.path!=""`))
 }
